9edd240c5ddf02042b9ee0abcde74bf1a696d593
wiwiec <derwiwie@googlemail.com>
2016-01-04 14:40:29 +0100

    added most associated things

--- ror_app/app/controllers/dings_controller.rb
+++ ror_app/app/controllers/dings_controller.rb
@@ -4,6 +4,18 @@ class DingsController < ApplicationController
 	
 	def index
 		@dings = Ding.all()
+
+		st = ActiveRecord::Base.connection
+		@pop_dings = st.execute('SELECT ass1.ding_id,ass1.count+ass2.count count FROM 
+				(SELECT ding_eins_id ding_id,count(*) as count 
+				FROM assoziations
+				GROUP BY ding_eins_id ORDER BY count DESC ) ass1 JOIN
+				(SELECT ding_zwei_id ding_id,count(*) as count 
+				FROM assoziations
+				GROUP BY ding_zwei_id ORDER BY count DESC ) ass2
+				ON (ass1.ding_id=ass2.ding_id)
+				ORDER BY count DESC LIMIT 10;')
+		st.close()
 	end
 
 	def show
