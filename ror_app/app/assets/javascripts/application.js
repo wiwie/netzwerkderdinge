f1819b11d84023cd356da8f2bc8117b28f0f80d8
wiwiec <derwiwie@googlemail.com>
2016-01-30 15:31:27 +0100

    added graphs to assoziations overview

--- ror_app/app/assets/javascripts/application.js
+++ ror_app/app/assets/javascripts/application.js
@@ -79,3 +79,55 @@ function remove_favorit_for_curr_user(user_id, ding_id) {
 $(document).ready(function() {
   $("a.fancybox").fancybox();
 });
+
+function renderGraph(graph, containerId) {
+    var graphics = Viva.Graph.View.svgGraphics();
+    graphics.node(function(node) {
+      //console.log(node);
+         // The function is called every time renderer needs a ui to display node
+         var link = Viva.Graph.svg('a')
+               .link(node.data.url);
+         var circle = Viva.Graph.svg('circle')
+               .attr('r', 5+Math.log(node.data.size))
+               .attr('fill', '#428bca')
+               .attr('stroke', '#FFFFFF')
+               .attr('stroke-width', '2px');
+         link.append(circle);
+               
+      var text = Viva.Graph.svg('text')
+        .attr('style', 'fill: #000000;')
+        .attr('opacity','1')
+          .text(node.data.label);
+      var ui = Viva.Graph.svg("g");
+      ui.append(link);
+      //ui.append(circle);
+      ui.append(text);
+      return ui;
+      })
+      .placeNode(function(nodeUI, pos){
+          // Shift image to let links go to the center:
+      var circle = nodeUI.childNodes[0].childNodes[0];
+      var radius = Number(circle.attr('r'));
+      var text = nodeUI.childNodes[1];
+
+      circle.attr('cx', pos.x).attr('cy', pos.y);
+      text.attr('x', pos.x + radius + 5).attr('y', pos.y);
+      });
+
+            
+    layout = Viva.Graph.Layout.forceDirected(graph, {
+        springLength : 10,
+        springCoeff : 0.00005,
+        dragCoeff : 0.02,
+        gravity : -5,
+        stableThreshold: graph.getNodesCount()
+    });
+
+    var renderer = Viva.Graph.View.renderer(graph, {
+      layout: layout,
+      graphics: graphics,
+      interactive: 'node drag scroll',
+      container: document.getElementById(containerId)
+    });
+    renderer.run();
+}
