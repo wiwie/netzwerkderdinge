7a4164163a0fbc0a9ba71511abcf22f74423891d
wiwiec <derwiwie@googlemail.com>
2016-02-10 02:53:53 +0100

    habit now shows a calendar-like view

--- ror_app/app/models/ding.rb
+++ ror_app/app/models/ding.rb
@@ -163,25 +163,6 @@ class Ding < ActiveRecord::Base
 
 		@times_done = @times_done.sort_by {|x| x.ding_zwei.name}
 
-		@has_latest = @times_done.count > 0
-		if @has_latest
-			@latest_ding = @times_done.last.ding_zwei
-		else
-			@latest_ding = @starttp_ding
-		end
-		begin
-			if @latest_ding.name.include? ':'
-				@latest_time = Time.strptime(@latest_ding.name, "%Y/%m/%d %H:%M")
-			else
-				@latest_time = Time.strptime(@latest_ding.name, "%Y/%m/%d")
-			end
-		rescue
-			return nil
-		end
-
-		@overdue = (Time.now - @latest_time - @ts)
-		@is_overdue = @overdue > 0
-
 		# how many times have we done the habit in time?
 		@streak = 0
 		comp_time = Time.now
@@ -214,39 +195,9 @@ class Ding < ActiveRecord::Base
 		@month = []
 		@week = []
 
-			# if its daily, show the last three months
-			# if @timespan_ding.name.end_with?(" day")
-			# 	current_day = Date.today
-			# 	current_day_ass_ind = @times_done.count-1
-			# 	current_date_ass = Date.parse(@times_done[current_day_ass_ind].ding_zwei.name)
-				 	
-			# 	while current_day > (Date.today-90.day)
-			# 		if current_day.wday == 0
-			# 			@month.append(@week)
-			# 			@week = []
-			# 		end
-
-			# 		if current_day.day == 1
-			# 			@last_months.append(@month)
-			# 			@month = []
-			# 		end
-
-			# 		while current_date_ass > current_day and current_day_ass_ind > 0
-			# 			current_day_ass_ind -= 1
-			# 			current_date_ass = Date.parse(@times_done[current_day_ass_ind].ding_zwei.name)
-			# 		end
-
-			# 		if current_date_ass == current_day
-			# 			@week.unshift([@times_done[current_day_ass_ind].ding_zwei.ding_typ.name,current_day.to_s])
-			# 		else
-			# 			@week.unshift(["",current_day.to_s])
-			# 		end
-
-			# 		current_day -= @ts
-			# 	end
-			# elsif @timespan_ding.name.end_with?(" week")
 		begin
 			if @starttp_ding and @ts
+				latest_date_done = nil
 				current_week = Time.parse(@starttp_ding.name)
 				if current_week < Date.today-90.day
 					current_week += ((Date.today-90.day-current_week)/@ts.to_f).ceil*@ts
@@ -258,13 +209,8 @@ class Ding < ActiveRecord::Base
 					current_date_ass = Time.parse(@times_done[current_day_ass_ind].ding_zwei.name)
 				end
 				 	
-				while current_week < Date.today
-					puts current_week
-					#if current_week.cweek % 4 == 0
-					#	@last_months.append(@month)
-					#	@month = []
-					#end
-					
+				while current_week < Time.now
+
 					while @times_done.count > 0 and current_date_ass < current_week and current_day_ass_ind < @times_done.count-1
 						current_day_ass_ind += 1
 						current_date_ass = Time.parse(@times_done[current_day_ass_ind].ding_zwei.name)
@@ -272,21 +218,26 @@ class Ding < ActiveRecord::Base
 
 					if @times_done.count > 0 and current_date_ass >= current_week and current_date_ass < current_week+@ts
 						@last_months.append([@times_done[current_day_ass_ind].ding_zwei.ding_typ.name, current_week.to_s])
+						latest_date_done = current_week+@ts
 					else
 						@last_months.append(["",current_week.to_s])
 					end
 
 					current_week += @ts
 				end
-				#if @month.count > 0
-				#	@last_months.append(@month)
-				#end
+
 				@last_months = [[@last_months]]
+				
+				@latest_time = latest_date_done
+			else
+				@latest_time = Time.parse(@starttp_ding.name)
 			end
 			#end
 		rescue
 			@last_months = []
 		end
+		@overdue = (Time.now - @latest_time - @ts)
+		@is_overdue = @overdue > 0
 
 
 		return {
